c926d5fa7fe6e8f62c814a9b69f2e011c1eac243
Stanislav <solo222@bk.ru>
2015-05-02 13:08:46 +0300

    First task

---
+++ Shylenok/src/lab1/HelloWorld.java
@@ -0,0 +1,10 @@
+package lab1;
+
+/**
+ * Created by USER on 02.05.2015.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
