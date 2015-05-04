ff5ec9e5349c95db426f3eab0e3d0799150add9f
Stanislav <solo222@bk.ru>
2015-05-05 00:24:49 +0300

    Task 3

---
+++ Shylenok/src/lab8/task8.java
@@ -0,0 +1,17 @@
+package lab8;
+
+/**
+ * Created by USER on 04.05.2015.
+ */
+public class task8 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 1;
+        int i =1;
+        while (i <=a ) {
+            b = b * i;
+            i++;
+        }
+        System.out.println(b);
+    }
+}
